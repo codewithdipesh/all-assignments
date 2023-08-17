df814699e5ec603435d6cca584e40de75cd9c84e
Dipesh Pal <121967041+codewithdipesh@users.noreply.github.com>
2023-08-17 19:27:48 +0530

    Done anagram.js

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -9,6 +9,21 @@
 
 function isAnagram(str1, str2) {
 
+  
+  //split to array because cant sort string
+  var array1 = str1.toLowerCase().split("");  //make it caseinsensitive BY (toLowerCase)
+  var array2 = str2.toLowerCase().split("");   //means (Dead Body = DeAD BOdY)=>SAME
+
+  //sort the arrays
+  array1 = array1.sort();
+  array2 = array2.sort();
+
+  //join 
+  var String1 = array1.join("");
+  var String2 = array2.join("");
+
+  return String1==String2;
+
 }
 
 module.exports = isAnagram;
